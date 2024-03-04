2bcdbf3b88a66d6bcd84b4773b63f46cfd1d2945
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-03-04 12:38:53 +0530

    Update and rename 33_Implement_Atoi.cpp to 33_Sequential_Digits.cpp

---
+++ 33_Sequential_Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    vector<int>ans;
+    void dfs(int low, int high, int i, int num){
+        if(num>=low and num<=high)
+            ans.push_back(num);
+        if(num>high or i>9)
+            return;
+        dfs(low,high,i+1,num*10+i);
+    }
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        for(int i=1;i<=9;i++)
+            dfs(low,high,i,0);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
