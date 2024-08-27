16673e318dec4043b6fda272804de6566d537928
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-08-27 19:41:52 +0530

    Update 240_Path_With_Maximum_Probability.cpp

--- 240_Path_With_Maximum_Probability.cpp
+++ 240_Path_With_Maximum_Probability.cpp
@@ -9,7 +9,6 @@ public:
                 int u = edges[j][0];
                 int v = edges[j][1];
                 double prob = succProb[j];
-
                 if (maxProb[u] * prob > maxProb[v]) {
                     maxProb[v] = maxProb[u] * prob;
                     updated = true;
